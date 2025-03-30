ca7efbaa25a6405e488699440334c336c724a20a
Avishka Kavishan <avishkakavishan13@gmail.com>
2025-03-30 08:12:17 +0530

    add button and inputBar

--- src/Screens/LogIn/LogIn.tsx
+++ src/Screens/LogIn/LogIn.tsx
@@ -1,14 +1,30 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
+import TextCom from '../../Components/text'
+import InpunField from '../../Components/InpunField'
+import Button from '../../Components/Button'
+import SocialB from '../../Components/SocialB'
 
 const LogIn = ({navigation}) => {
   return (
-    <View>
-      <Text>LogIn</Text>
+    <View style={styles.container}>
+      <TextCom lable={'Log In'} />
+
+      <InpunField placeholder={"E-mail"} keyboardType={"email"} />
+      <InpunField placeholder={'Password'} secureTextEntry={true} />
+
+      <Button lable={'Login'} onPress={() => navigation.navigate()}/>
+
+      <SocialB/>
+      
     </View>
   )
 }
 
-
+const styles = StyleSheet.create({
+    container:{
+        alignItems:'center',
+    }
+})
 
 export default LogIn
