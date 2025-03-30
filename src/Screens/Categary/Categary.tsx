cfaac96e23a16a128ced5343eb2253fb3d97a23a
Avishka Kavishan <avishkakavishan13@gmail.com>
2025-03-30 19:19:42 +0530

    add categary page

---
+++ src/Screens/Categary/Categary.tsx
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Categary = () => {
+
+  const categarys = [
+    {id: 1, title:"Destination", image: require("")}
+  ]
+
+  return (
+    <View>
+      <Text>Categary</Text>
+    </View>
+  )
+}
+
+export default Categary
